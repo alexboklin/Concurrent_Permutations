2ff7458f6c5aeed74df0ad6a83a5be9c4c9e7a75
Alex Boklin <alex.boklin@gmail.com>
2016-01-19 10:51:19 +0300

    add permTail to the mix

--- permutate.go
+++ permutate.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+/*
+rotate constructs "rotations" of a list. For instance,
+rotate [1,2,3,4] gives [[1,2,3,4],[2,3,4,1],[3,4,1,2],[4,1,2,3]]
+*/
 func rotate(input []int) [][]int {
 	return rotateHelper(input, [][]int{}, 0)
 }
@@ -14,7 +20,20 @@ func rotateHelper(src []int, dest [][]int, accumulator int) [][]int {
 		append(dest, [][]int{src}...), accumulator+1)
 }
 
+/*
+permTail gives permutations of the list's tail. For instance,
+permTail [1,2,3,4] gives [[1,2,3,4],[1,3,4,2],[1,4,2,3]]
+*/
+func permTail(input []int) [][]int {
+	result := make([][]int, 0, len(input[1:]))
+	for _, tailRrotation := range rotate(input[1:]){
+		result = append(result, append([]int{input[0]}, tailRrotation...))
+
+	}
+	return result
+}
+
 func main() {
-	// *rotate [1,2,3,4] gives [[1,2,3,4],[2,3,4,1],[3,4,1,2],[4,1,2,3]]
-	fmt.Println(rotate([]int{1, 2, 3, 4}))
+	fmt.Println("rotate [1,2,3,4]: ", rotate([]int{1, 2, 3, 4}))
+	fmt.Println("permTail [1,2,3,4]: ", permTail([]int{1, 2, 3, 4}))
 }
